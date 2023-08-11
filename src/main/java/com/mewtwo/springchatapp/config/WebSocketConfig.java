525870609275352de0eab4ac440fbde5edc0a0f0
majestymewtwo <majestymewtwo@gmail.com>
2023-08-11 19:02:54 +0530

    added a private message mapping

--- src/main/java/com/mewtwo/springchatapp/config/WebSocketConfig.java
+++ src/main/java/com/mewtwo/springchatapp/config/WebSocketConfig.java
@@ -17,5 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
     }
 }
--- src/main/java/com/mewtwo/springchatapp/controller/ChatController.java
+++ src/main/java/com/mewtwo/springchatapp/controller/ChatController.java
@@ -2,31 +2,36 @@ package com.mewtwo.springchatapp.controller;
 
 import com.mewtwo.springchatapp.dto.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.util.HtmlUtils;
 
+import java.security.Principal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @CrossOrigin
 @RequiredArgsConstructor
 public class ChatController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
     public List<Message> messages = new ArrayList<>();
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public List<Message> greeting(Message message) throws Exception {
+    public List<Message> greeting(@Payload Message message, @Header("simpSessionId") String sessionId) throws Exception {
         Thread.sleep(100);
         Message response = new Message();
         response.setMessage(message.getMessage());
         messages.add(response);
         return messages;
+    }
+    @MessageMapping("/user-message")
+    public Message sendMessage(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiver(), "/private-chat", message);
+        return message;
     }
 }
